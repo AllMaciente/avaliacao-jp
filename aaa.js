d6ed191efdd581fb9f701bdab4c46bee63b788b6
Allan <135379484+AllMaciente@users.noreply.github.com>
2024-08-23 21:40:38 -0300

    a

--- aaa.js
+++ aaa.js
@@ -1,6 +1,7 @@
 function addItem() {
   var par = document.getElementById("add");
   var h1 = document.createElement("h1");
-  var input = document.getElementById('tarefa')
-  h1.textContent = 
+  var input = document.getElementById("tarefa").value;
+  h1.textContent = input;
+  par.appendChild(h1);
 }
